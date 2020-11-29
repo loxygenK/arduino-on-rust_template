6bb6ce43884847c70e1a0cbd4268b6019947f7c5
loxygenK <loxygen.k@gmail.com>
2020-11-29 10:53:25 +0900

    Implement main function

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,30 @@
 // Use crate 'panic_halt' to use it as panic handler.
 extern crate panic_halt;
 
+// Import some stuffs from arduino_uno.
+use arduino_uno::{Peripherals, Pins};
+use arduino_uno::prelude::*;
+
 // Here is our entrypoint.
 #[arduino_uno::entry]
 fn main() -> ! {
+    
+    // Get the peripherals to access pin or some stuff
+    let phr = Peripherals::take().unwrap();
+    
+    // Get pins and associate it to our peripherals...
+    let mut pins = Pins::new(phr.PORTB, phr.PORTC, phr.PORTD);
+    // And get D13 pin which is connected to the LED 'L' in arduino,
+    // and make it output.
+    let mut internal_led = pins.d13.into_output(&mut pins.ddr);
+    
+    // Now the initialization is done!
+    // Here is the main routine.
     loop {
-
+        // Toggle the LED! What a useful method.
+        internal_led.toggle().void_unwrap();
+        
+        // Wait a moment.
+        arduino_uno::delay_ms(100);
     }
 }
